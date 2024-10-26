fa873a2a58ddb517a585b6f987377a3d779f5875
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2024-10-26 11:19:47 -0700

    feat: Added question retrival endpoint

---
+++ Backend/index.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Define a GET endpoint
+app.get('/', (req, res) => {
+  res.send('Hello, this is your GET endpoint!');
+});
+
+// Answer
+// Full String Question
+// First Number
+// Second Number
+// Operator
+
+class Question{
+  constructor(full,num1,num2,ans,op){
+    this.fullString=full;
+    this.firstNumber=num1;
+    this.secondNumber=num2;
+    this.answer=ans;
+    this.operator=op;
+  }
+}
+
+function getRandomInt(max) {
+  n = Math.floor(Math.random() * max);
+  // console.log(n)
+  return n;
+}
+
+function getDivisionQuestion(multiplier=1) {
+  let secondNum = getRandomInt(1000)*multiplier;
+  let answer = getRandomInt(100)*multiplier;
+  let firstNum = answer * secondNum;
+  return new Question(
+    `${firstNum} / ${secondNum} = ${answer}`,
+    firstNum,
+    secondNum,
+    answer,
+    '/'
+  )
+}
+
+function getMultiplcationQuestion(multiplier=1) {
+  let firstNum = getRandomInt(1000)*multiplier;
+  let secondNum = getRandomInt(1000)*multiplier;
+  let answer = firstNum * secondNum;
+  return new Question(
+    `${firstNum} * ${secondNum} = ${answer}`,
+    firstNum,
+    secondNum,
+    answer,
+    'x'
+  ) 
+}
+
+function getAdditionQuestion(multiplier=1) {
+  let firstNum = getRandomInt(1000)*multiplier;
+  let secondNum = getRandomInt(1000)*multiplier;
+  let answer = firstNum + secondNum;
+  return new Question(
+    `${firstNum} + ${secondNum} = ${answer}`,
+    firstNum,
+    secondNum,
+    answer,
+    '+'
+  ) 
+}
+
+function getSubtractionQuestion(multiplier=1) {
+  let firstNum = getRandomInt(1000)*multiplier;
+  let secondNum = getRandomInt(1000)*multiplier;
+  let answer = firstNum - secondNum;
+  return new Question(
+    `${firstNum} - ${secondNum} = ${answer}`,
+    firstNum,
+    secondNum,
+    answer,
+    '-'
+  ) 
+}
+
+const questionGenerators = [getMultiplcationQuestion, getDivisionQuestion, getAdditionQuestion, getSubtractionQuestion];
+
+function getRandomQuestion() {
+  let q = questionGenerators[getRandomInt(questionGenerators.length-1)];
+  console.log(q());
+  return q;
+}
+
+// Define a GET endpoint
+app.get('/question', (req, res) => {
+  const { amount } = req.query;
+  let questions = [];
+  let i = 0;
+  
+  while (i < amount) {
+    questions.push(getRandomQuestion()())
+    i++;
+  }
+  
+  res.send(questions)
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
