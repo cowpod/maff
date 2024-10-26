4ea3fb7761a33b4e48d8f9d89fd005e98e8d4986
Henry Gross-Hellsen <6283258+cowpod@users.noreply.github.com>
2024-10-26 13:46:30 -0700

    database.js, gitignore

--- Backend/database.js
+++ Backend/database.js
@@ -11,8 +11,11 @@ class Database {
         });
     }
 
-    createTable(tableName, columns) {
-        const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${columns})`;
+/*    createTable(tableName, columns) {
+        const columnsStr = columns.map(column => {
+            return `${column.name} ${column.type}${column.primaryKey ? ' PRIMARY KEY' : ''}${column.notNull ? ' NOT NULL' : ''}${column.default ? ` DEFAULT ${column.default}` : ''}`;
+        }).join(', ');
+        const sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${columnsStr})`;
         return new Promise((resolve, reject) => {
             this.db.run(sql, (err) => {
                 if (err) {
@@ -23,7 +26,7 @@ class Database {
             });
         });
     }
-
+        
     insert(tableName, columns, values) {
         const sql = `INSERT INTO ${tableName} (${columns}) VALUES (${values})`;
         return new Promise((resolve, reject) => {
@@ -45,6 +48,30 @@ class Database {
                     reject(err);
                 } else {
                     resolve(rows);
+                }
+            });
+        });
+    } */
+
+    query(sql) {
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    execute(sql) {
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, [], (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
                 }
             });
         });
